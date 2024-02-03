4538f289ae6fc0f037657d6d455b37d141d08069
Tran Duong <109237079+ScandiK2@users.noreply.github.com>
2024-02-04 00:04:20 +0700

    Add files via upload
    Thêm các file để tạo form Hóa đơn

---
+++ QLGARA/fHoaDon.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace QLGARA
+{
+    partial class fHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            inHoaDon = new Button();
+            nutTimKiem = new Button();
+            comboBox1 = new ComboBox();
+            panel2 = new Panel();
+            groupBox1 = new GroupBox();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            textMaHD = new TextBox();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            panel3 = new Panel();
+            groupBox2 = new GroupBox();
+            textBox9 = new TextBox();
+            label13 = new Label();
+            dataGridViewHoaDon = new DataGridView();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            panel3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewHoaDon).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Mã hóa đơn:";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(inHoaDon);
+            panel1.Controls.Add(nutTimKiem);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(50, 473);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(990, 53);
+            panel1.TabIndex = 1;
+            // 
+            // inHoaDon
+            // 
+            inHoaDon.Location = new Point(658, 18);
+            inHoaDon.Name = "inHoaDon";
+            inHoaDon.Size = new Size(140, 30);
+            inHoaDon.TabIndex = 3;
+            inHoaDon.Text = "In hóa đơn";
+            inHoaDon.UseVisualStyleBackColor = true;
+            // 
+            // nutTimKiem
+            // 
+            nutTimKiem.Location = new Point(494, 17);
+            nutTimKiem.Name = "nutTimKiem";
+            nutTimKiem.Size = new Size(120, 32);
+            nutTimKiem.TabIndex = 2;
+            nutTimKiem.Text = "Tìm kiếm";
+            nutTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(136, 20);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(292, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(groupBox1);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(51, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(988, 250);
+            panel2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textMaHD);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(17, 46);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(949, 182);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin chung";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(732, 60);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(161, 27);
+            textBox8.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(712, 26);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(186, 27);
+            textBox7.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(416, 126);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(164, 27);
+            textBox6.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(416, 93);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(146, 27);
+            textBox5.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(476, 60);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(184, 27);
+            textBox4.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(476, 29);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(131, 134);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(158, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(131, 95);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(158, 28);
+            comboBox2.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(131, 62);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(158, 27);
+            dateTimePicker1.TabIndex = 11;
+            // 
+            // textMaHD
+            // 
+            textMaHD.Location = new Point(131, 29);
+            textMaHD.Name = "textMaHD";
+            textMaHD.Size = new Size(158, 27);
+            textMaHD.TabIndex = 10;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(668, 62);
+            label12.Name = "label12";
+            label12.Size = new Size(58, 20);
+            label12.TabIndex = 9;
+            label12.Text = "Địa chỉ:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(668, 32);
+            label11.Name = "label11";
+            label11.Size = new Size(38, 20);
+            label11.TabIndex = 8;
+            label11.Text = "SDT:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(358, 126);
+            label10.Name = "label10";
+            label10.Size = new Size(54, 20);
+            label10.TabIndex = 7;
+            label10.Text = "Tên xe:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(358, 92);
+            label9.Name = "label9";
+            label9.Size = new Size(52, 20);
+            label9.TabIndex = 6;
+            label9.Text = "Mã xe:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(358, 62);
+            label8.Name = "label8";
+            label8.Size = new Size(114, 20);
+            label8.TabIndex = 5;
+            label8.Text = "Tên khách hàng:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(358, 32);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 20);
+            label7.TabIndex = 4;
+            label7.Text = "Mã khách hàng:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(23, 137);
+            label6.Name = "label6";
+            label6.Size = new Size(102, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Tên nhân viên:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(25, 98);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Mã nhân viên:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 67);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Ngày xuất đơn:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(33, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Mã hóa đơn:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(396, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 20);
+            label2.TabIndex = 0;
+            label2.Text = "HÓA ĐƠN THANH TOÁN";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(groupBox2);
+            panel3.Location = new Point(51, 281);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(990, 179);
+            panel3.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(dataGridViewHoaDon);
+            groupBox2.Location = new Point(24, 14);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(940, 154);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Dịch vụ sử dụng";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(725, 59);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(199, 27);
+            textBox9.TabIndex = 2;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(633, 59);
+            label13.Name = "label13";
+            label13.Size = new Size(75, 20);
+            label13.TabIndex = 1;
+            label13.Text = "Tổng tiền:";
+            // 
+            // dataGridViewHoaDon
+            // 
+            dataGridViewHoaDon.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewHoaDon.Location = new Point(24, 26);
+            dataGridViewHoaDon.Name = "dataGridViewHoaDon";
+            dataGridViewHoaDon.RowHeadersWidth = 51;
+            dataGridViewHoaDon.Size = new Size(576, 122);
+            dataGridViewHoaDon.TabIndex = 0;
+            dataGridViewHoaDon.Click += dataGridViewHoaDon_Click;
+            // 
+            // fHoaDon
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1072, 538);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "fHoaDon";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Hoa Don";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            panel3.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewHoaDon).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Button nutTimKiem;
+        private ComboBox comboBox1;
+        private Panel panel2;
+        private Label label2;
+        private Panel panel3;
+        private GroupBox groupBox1;
+        private Label label4;
+        private Label label3;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textMaHD;
+        private Label label12;
+        private Label label11;
+        private TextBox textBox2;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private GroupBox groupBox2;
+        private Label label13;
+        private DataGridView dataGridViewHoaDon;
+        private TextBox textBox9;
+        private Button inHoaDon;
+        private ComboBox comboBox2;
+    }
+}
---
+++ QLGARA/fHoaDon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLGARA.DAO;
+using QLGARA.DTO;
+using System.Data.SqlClient;
+using COMExcel = Microsoft.Office.Interop.Excel;
+using System.Numerics;
+using QLGARA.Class;
+
+
+namespace QLGARA
+{
+    public partial class fHoaDon : Form
+    {
+        DataTable tblCTHD;
+
+        public fHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void fHoaDon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Nap du lieu len Datagrid
+        private void LoadDataGridView()
+        {
+            string sql;
+            sql = "SELECT *FROM dbo.HoaDon";
+           // tblCTHD = Functions.GetDataToTable(sql);
+        }
+
+        private void dataGridViewHoaDon_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
